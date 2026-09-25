90038a3f62d54d1743451be4ed79575014e76f6e
PriyaK196 <priyu.6363@gmail.com>
2026-09-25 15:06:41 +0530

    Create solution.py

---
+++ 03-Time-Conversion/solution.py
@@ -0,0 +1,16 @@
+def timeConversion(s):
+    hour = int(s[:2])
+    period = s[-2:]
+
+    if period == "AM":
+        if hour == 12:
+            hour = 0
+    else:
+        if hour != 12:
+            hour += 12
+
+    return f"{hour:02d}{s[2:-2]}"
+
+
+s = input()
+print(timeConversion(s))
