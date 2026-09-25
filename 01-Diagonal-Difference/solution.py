fd8a31ade8f8b4ffa66d4ee70de48765f91472c4
PriyaK196 <priyu.6363@gmail.com>
2026-09-25 14:28:18 +0530

    Update solution.py

--- 01-Diagonal-Difference/solution.py
+++ 01-Diagonal-Difference/solution.py
@@ -1,1 +1,22 @@
+def diagonalDifference(arr):
+    n = len(arr)
+
+    primary = 0
+    secondary = 0
+
+    for i in range(n):
+        primary += arr[i][i]
+        secondary += arr[i][n - 1 - i]
+
+    return abs(primary - secondary)
+
+
+n = int(input())
+
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+print(diagonalDifference(arr))
 
